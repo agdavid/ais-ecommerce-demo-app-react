ea3ceee8969e21e455d9cc6cd3d304c7a66e428d
Ajay David <ajaydavid@ANTONIO-ALGOLIA-MBP.local>
2020-10-22 14:59:27 -0400

    custom searchbox and hit selector

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import {
   ClearRefinements,
   RefinementList,
   Configure,
-  connectHighlight
+  connectHighlight,
+  createConnector
 } from 'react-instantsearch-dom';
 import PropTypes from 'prop-types';
 import './App.css';
@@ -32,7 +33,11 @@ class App extends Component {
             <Configure hitsPerPage={8}/>
           </div>
           <div className="right-panel">
-            <SearchBox />
+            {/* <SearchBox /> */}
+            <label>SearchBox</label>
+            <ConnectedSearchBox />
+            <label>Hit Count:</label>
+            <ConnectedHitSelector />
             <Hits hitComponent={Hit}/>
             <Pagination />
           </div>
@@ -42,12 +47,91 @@ class App extends Component {
   }
 }
 
-function Hit(props) {
+// to create a custom widget
+// use createConnector to create your custom widget function
+
+const connectWithQuery = createConnector({
+  // give your component a name
+  displayName: 'CustomWidgetWithQuery',
+  // what props to forward to the composed component
+  getProvidedProps(props, searchState) {
+    // searchState is the state object
+    // add a new state attribute for your custom data
+    const currentRefinement = searchState.attributeForMyQuery || '';
+    // you now have a searchState object like
+    // { attributeForMyQuery: '' }
+    return { currentRefinement }
+
+  },
+  refine(props, searchState, nextRefinement) 
+  {
+    // update the search state 
+    return { 
+      ...searchState,
+      attributeForMyQuery: nextRefinement
+    }
+  },
+  getSearchParameters(searchParameters, props, searchState) {
+    return searchParameters.setQuery(searchState.attributeForMyQuery || '');
+  },
+  cleanUp(props, searchState) {
+    const { attributeForMyQuery, ...nextSearchState } = searchState;
+    return nextSearchState;
+  }
+})
+
+const MySearchBox = (props) => (
+  <input 
+    type="input"
+    value={props.currentRefinement}
+    onChange={ e => props.refine(e.currentTarget.value)}
+  />
+);
+
+const ConnectedSearchBox = connectWithQuery(MySearchBox);
+
+const connectWithHit = createConnector({ 
+  displayName: 'CustomHitSelectorWidget',
+  getProvidedProps(props, searchState, searchResults) {
+    // this sends props to the composed component
+    // add your custom data from the widget to searchState
+    const currentHitsPerPage = searchState.attributeForHitsPerPage || '';
+    return { currentHitsPerPage };
+  },
+  refine(props, searchState, nextRefinement) {
+    // on refine
+    // update search state with new data from component
+    return {
+      ...searchState,
+      attributeForHitsPerPage: nextRefinement
+    }
+  },
+  getSearchParameters(searchParameters, props, searchState) {
+    // update the search parameters with new search state data 
+    return searchParameters.setQueryParameter('hitsPerPage', searchState.attributeForHitsPerPage || 8);
+  },
+  cleanUp(props, searchState) {
+    const { currentHitsPerPage, ...nextSearchState} = searchState;
+    return nextSearchState;
+  }
+})
+
+const MyHitSelector = (props) => (
+  <input 
+  type="input"
+  value={props.currentHitsPerPage}
+  onChange={e => props.refine(e.currentTarget.value)}
+  />
+)
+
+const ConnectedHitSelector = connectWithHit(MyHitSelector)
+
+const Hit = (props) => {
   return (
-    <div key={props.hit.objectID}>
+    <div>
       <img src={props.hit.image} align="left" alt={props.hit.name} />
       <div className="hit-name">
-        <CustomHighlight attribute="name" hit={props.hit} />
+        <CustomHighlight attribute="name" hit={props.hit}/>
       </div>
       <div className="hit-description">
         <Highlight attribute="description" hit={props.hit} />
@@ -64,8 +148,6 @@ const CustomHighlight = connectHighlight(({ highlight, attribute, hit }) => {
     attribute,
     hit
   });
-
-  console.log(parsedHit)
 
   return(
     <div>
